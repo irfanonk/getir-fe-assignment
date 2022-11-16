776c9993bc2965337815339bd85414cc521c21ce
irfanonk <onk.irfan@gmail.com>
2022-11-16 15:13:09 +0300

    rm

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -2,10 +2,9 @@ import React, { PropsWithChildren } from "react";
 import { render } from "@testing-library/react";
 import type { RenderOptions } from "@testing-library/react";
 import { configureStore } from "@reduxjs/toolkit";
-import type { PreloadedState } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
-import type { AppStore, RootState } from "../redux/store";
+import type { AppStore } from "../redux/store";
 // As a basic setup, import your same slice reducers
 
 import itemSlice from "../features/items/itemSlice";
